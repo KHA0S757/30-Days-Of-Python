f4a12898421ac2221e6b51208a89e859295b9528
KHA0S757 <khaoskitty@live.com>
2025-07-02 23:30:06 -0400

    Day 1 Completed Exercises

---
+++ 01_Day_Introduction/day_1/Euclidean_Exercise.py
@@ -0,0 +1,11 @@
+##Euclidean Formula
+##d(p,q)**2=(p1-q1)**2+(p2-q2)**2
+import math
+print("Given Coordinates (2,3) and (10,8)")
+p1=2
+p2=10
+q1=3
+q2=8
+x=((p2-p1)**2)
+y=((q2-q1)**2)
+print(math.sqrt(x+y))
